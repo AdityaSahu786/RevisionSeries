3f65c45f70f38abe1fe31a9b1627bf9d937f534a
AdityaSahu786 <sahuadi786183@gmail.com>
2024-10-19 23:46:59 +0530

    Added First Component

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <header>
-            <h1>You have 3 open tasks </h1>
+            <h1 className="text-gradient">You have 3 open tasks </h1>
         </header>
     )
 }
