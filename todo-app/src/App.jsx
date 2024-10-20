6f7ae14e2936d03a64772fcc3d47c3b089fdaa96
AdityaSahu786 <sahuadi786183@gmail.com>
2024-10-20 22:59:43 +0530

    Setting up props

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -6,7 +6,7 @@ import { TodoList } from "./components/TodoList"
 function App() {
       const todos = [
         { input: 'Hello! Add your first todo!', complete: true},
-        { input: 'Get the groceries', complete: false},
+       { input: 'Get the groceries', complete: false},
         { input: 'Learn how to web design', complete: false},
         { input: 'Say hi to gran gran', complete: true},
       ]
@@ -16,8 +16,8 @@ function App() {
   return (
     <>
       <Header todos={todos}/>
-      <Tabs />
-      <TodoList />
+      <Tabs  todos={todos}/>
+      <TodoList todos={todos}/>
       <TodoInput/>
     </>
   )
