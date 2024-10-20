9bdc295f77dfb78e7848ccfddc48d8349fc73eec
AdityaSahu786 <sahuadi786183@gmail.com>
2024-10-20 23:36:09 +0530

    Adding FilterTodoList

--- todo-app/src/components/TodoCard.jsx
+++ todo-app/src/components/TodoCard.jsx
@@ -6,7 +6,7 @@ export function TodoCard(props) {
         <div className="card todo-item">
         <p>{todo.input}</p>
         <div className="todo-buttons">
-            <button>
+            <button disabled={todo.complete}>
                 <h6>Done</h6>
             </button>
             <button>
--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -4,11 +4,16 @@ export function TodoList(props) {
     const { todos } = props
 
     const tab = 'All'
+    const filterTodoList = tab === 'All' ?
+     todos :
+     tab === 'Completed' ?
+       todos.filter(val => val.complete) :
+       todos.filter(val => !val.complete)
 
 
     return (
         <>
-             {todos.map((todo, todoIndex) => {
+             {filterTodoList.map((todo, todoIndex) => {
                 return (
                     <TodoCard key={todoIndex} todoIndex={todoIndex} {...props} />
                 )
